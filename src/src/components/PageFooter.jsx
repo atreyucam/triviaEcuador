11698cab64c738020ac9e16e738ee632f92715d9
JCMostache <justin.constante@espoch.edu.ec>
2024-07-18 02:01:20 -0500

    Himno EC

--- src/src/components/PageFooter.jsx
+++ src/src/components/PageFooter.jsx
@@ -7,46 +7,58 @@ import { MdInfo } from 'react-icons/md'
 import { GoAlert } from 'react-icons/go'
 import { BsFillStarFill } from 'react-icons/bs'
 
+// Crear una variable de audio global para manejar la reproducción del himno
+const himnoAudio = typeof Audio !== "undefined" && new Audio('/sounds/himnoec.mp3');
+
 export default function Footer ({ alert = false }) {
-	const [sound, setSound] = useState(false)
-	const [showInfo, setShowInfo] = useState(false)
+    const [sound, setSound] = useState(false)
+    const [showInfo, setShowInfo] = useState(false)
 
-	useEffect(() => {
-		if (!localStorage.getItem('sound')) localStorage.setItem('sound', sound)
-		else setSound(localStorage.getItem('sound') === 'true')
-	}, [])
+    useEffect(() => {
+        if (!localStorage.getItem('sound')) localStorage.setItem('sound', sound)
+        else setSound(localStorage.getItem('sound') === 'true')
+    }, [])
 
-	useEffect(() => localStorage.setItem('sound', sound), [sound])
+    useEffect(() => {
+        localStorage.setItem('sound', sound)
+        // Manejar la reproducción del himno basado en el estado del sonido
+        if (sound) {
+            himnoAudio.play();
+        } else {
+            himnoAudio.pause();
+            himnoAudio.currentTime = 0; // Reiniciar la canción si se apaga el sonido
+        }
+    }, [sound])
 
-	function handleClick (info = false) {
-		info ? setShowInfo(!showInfo) : setSound(!sound)
-		playSound('switch-on')
-	}
+    function handleClick (info = false) {
+        info ? setShowInfo(!showInfo) : setSound(!sound)
+        playSound('switch-on')
+    }
 
-	function handleSoundON () {
-		setSound(true)
-		localStorage.setItem('sound', true)
-		playSound('switch-on')
-	}
+    function handleSoundON () {
+        setSound(true)
+        localStorage.setItem('sound', true)
+        playSound('switch-on')
+    }
 
-	return (
-		<footer className='fixed right-4 bottom-3 z-20'>
-			<nav>
-				<ul className='flex gap-4'>
-					<li className='relative'>
-					</li>
+    return (
+        <footer className='fixed right-4 bottom-3 z-20'>
+            <nav>
+                <ul className='flex gap-4'>
+                    <li className='relative'>
+                    </li>
 
-					<li>
-						<button title={sound ? 'Mute' : 'Play music'} className='align-middle hover:scale-105 p-1.5 bg-white rounded-md'>
-							{
-								sound
-									? <Image src={soundOn} alt="" width={25} height={25} onClick={() => setSound(false)} />
-									: <Image src={soundOff} alt="" width={25} height={25} onClick={handleSoundON} />
-							}
-						</button>
-					</li>
-				</ul>
-			</nav>
-		</footer>
-	)
+                    <li>
+                        <button title={sound ? 'Mute' : 'Play music'} className='align-middle hover:scale-105 p-1.5 bg-white rounded-md'>
+                            {
+                                sound
+                                    ? <Image src={soundOn} alt="" width={25} height={25} onClick={() => setSound(false)} />
+                                    : <Image src={soundOff} alt="" width={25} height={25} onClick={handleSoundON} />
+                            }
+                        </button>
+                    </li>
+                </ul>
+            </nav>
+        </footer>
+    )
 }
